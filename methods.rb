59b756a7b9e9f65f923e430f6f8b2301c9d5167f
afeinber <alexandernfeinberg@gmail.com>
2014-06-12 10:34:39 -0400

    finished assessment

--- methods.rb
+++ methods.rb
@@ -6,7 +6,7 @@ end
 
 y = square(2)
 # What is the value of 'x' after this line executes?
-
+x is undefined
 
 #### Question 2
 # What is the value of `age` after we call `get_older`?
@@ -17,6 +17,8 @@ end
 
 age = 10
 get_older(age)
+
+age  is still 10
 
 
 #### Question 3
@@ -34,11 +36,17 @@ end
 energy = 2
 jump
 
+I believe that this will be a syntax error since energy is not given a value for how_high
+
 
 #### Question 4
 # Write a method that accepts a string and an optional multiplier (defaults to 3)
 # and returns the original string multiplied that many times (using the * operator).
 # Do not use an options hash or keyword arguments.
+
+def mult_string(str, mult = 3)
+  mult * str
+end
 
 
 #### Question 5
@@ -48,3 +56,6 @@ def greeting(name:, salutation: 'Hi', volume: :low)
   greeting = "#{salutation} there, #{name}"
   volume == :high ? greeting.upcase : greeting
 end
+
+
+greeting(name: 'puny humans', salutation: 'hello', volume: :high)
